28fea1688ecf17dfb534ed574c255bc39b0adfd1
myowin76 <myowin76@gmail.com>
2013-06-10 17:12:33 +0100

    static assets test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,10 @@ class HomeController < ApplicationController
   	@gtins = Gtin.paginate(:page => params[:page], :per_page => 30)
     # debugger
   	doc = Nokogiri::XML(File.open("#{Rails.root}/public/nestle.xml")) 
+
+    # doc = Nokogiri::XML(File.open("#{asset_path}/nestle.xml")) 
   	@products = doc.xpath("//Product")
+    debugger
   	# @existing_products = Product.all
   	# debugger
   	# @products.each do |product|
